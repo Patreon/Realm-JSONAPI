c598604f717834d4746db555a549f1991a8446c7
David Kettler <21echoes@gmail.com>
2016-05-16 00:53:30 -0700

    clean up JSONAPIParser code and fix a bug where we hard-coded id as the primary key

--- RLMObject-JSONAPI/Classes/RLMObject+JSONAPIParser.m
+++ RLMObject-JSONAPI/Classes/RLMObject+JSONAPIParser.m
@@ -12,86 +12,148 @@
   RLMSchema           *schema = realm.schema;
   RLMObjectSchema     *objectSchema = schema[NSStringFromClass([self class])];
 
-  for (NSString *jsonPath in map) {
-    NSString    *modelProperty = map[jsonPath];
+  [self parseIDAndAttributesFromJSON:json
+                     intoModelValues:model
+                            forRealm:realm
+                 usingJSONtoModelMap:map
+                     andObjectSchema:objectSchema];
+
+  [self parseRelationshipsFromJSON:json
+                   intoModelValues:model
+                          forRealm:realm
+               usingJSONtoModelMap:map
+                   andObjectSchema:objectSchema];
+
+  return model;
+}
+
++ (void)parseIDAndAttributesFromJSON:(NSDictionary *)json
+                     intoModelValues:(NSMutableDictionary *)model
+                            forRealm:(RLMRealm *)realm
+                 usingJSONtoModelMap:(NSDictionary *)jsonToModelMap
+                     andObjectSchema:(RLMObjectSchema *)objectSchema
+{
+  for (NSString *jsonPath in jsonToModelMap) {
+    id value = [self getValueFromJSON:json
+                               atPath:jsonPath];
+    if (!value || value == [NSNull null]) {
+      continue;
+    }
+
+    NSString    *modelProperty = jsonToModelMap[jsonPath];
     RLMProperty *realmProperty = objectSchema[modelProperty];
-
-    NSString *fullJSONPath = jsonPath;
-    NSMutableArray *jsonPathComponents = [NSMutableArray arrayWithCapacity:3];
-    if (![fullJSONPath isEqualToString:@"id"]) {
-      [jsonPathComponents addObject:@"attributes"];
-    }
-    [jsonPathComponents addObjectsFromArray:[fullJSONPath componentsSeparatedByString:@"."]];
-    id value = json;
-    for (NSString *component in jsonPathComponents) {
-      value = [value objectForKey:component];
-      if (!value || value == [NSNull null]) {
-        break;
-      }
-    }
-    if (value && value != [NSNull null]) {
-      if (!realmProperty) {
-        // Here we have a JSON property with no equivalent model property
-        continue;
-      } else if (realmProperty.type == RLMPropertyTypeObject) {
-        Class propertyClass = NSClassFromString(realmProperty.objectClassName);
-        RLMObject *realmObject = [self realmObjectInRealm:realm withClass:propertyClass andUID:[value stringValue]];
-        if (realmObject) {
-          model[modelProperty] = realmObject;
-        }
-      } else if (realmProperty.type == RLMPropertyTypeString) {
-        NSString *stringValue = nil;
-        if ([value isKindOfClass:[NSString class]]) {
-          stringValue = (NSString *)value;
-        } else if ([value respondsToSelector:@selector(stringValue)]) {
-          stringValue = [value stringValue];
-        } else {
-          stringValue = [NSString stringWithFormat:@"%@", value];
-        }
-        if (stringValue) {
-          model[modelProperty] = stringValue;
-        }
-      } else if (realmProperty.type == RLMPropertyTypeDate) {
-        NSDate *date = [NSDate dateFromISO8601String:value];
-        if (date) {
-          model[modelProperty] = date;
-        }
-      } else {
-        model[modelProperty] = value;
-      }
-    }
-  }
-  NSDictionary *links = json[@"relationships"];
-  for (NSDictionary *property in links.keyEnumerator) {
-    NSDictionary *link = links[property];
-    NSString     *modelProperty = map[property];
-    RLMProperty  *realmProperty = objectSchema[modelProperty];
-    Class propertyClass = NSClassFromString(realmProperty.objectClassName);
-
     if (!realmProperty) {
       // Here we have a JSON property with no equivalent model property
       continue;
     }
-    else if (link[@"data"]) {
-      if ([link[@"data"] isKindOfClass:[NSDictionary class]]) {
-        NSString *uid = link[@"data"][@"id"];
-        id linkedObject = [self realmObjectInRealm:realm withClass:propertyClass andUID:uid];
-        model[modelProperty] = linkedObject;
-      } else if ([link[@"data"] isKindOfClass:[NSArray class]]) {
-        NSMutableArray *linkedObjects = [NSMutableArray array];
-        for (id linkageItem in link[@"data"]) {
-          NSString *uid = linkageItem[@"id"];
-          id linkedObject = [self realmObjectInRealm:realm withClass:propertyClass andUID:uid];
-          [linkedObjects addObject:linkedObject];
-        }
-        model[modelProperty] = linkedObjects;
-      } else {
-        // TODO: linkage is null. what to do with it?
+
+    if (realmProperty.type == RLMPropertyTypeObject)
+    {
+      Class propertyClass = NSClassFromString(realmProperty.objectClassName);
+      RLMObject *realmObject = [self realmObjectInRealm:realm withClass:propertyClass andUID:[value stringValue]];
+      if (realmObject) {
+        model[modelProperty] = realmObject;
       }
     }
-    // end API hacks
+    else if (realmProperty.type == RLMPropertyTypeString)
+    {
+      NSString *stringValue = nil;
+      if ([value isKindOfClass:[NSString class]]) {
+        stringValue = (NSString *)value;
+      } else if ([value respondsToSelector:@selector(stringValue)]) {
+        stringValue = [value stringValue];
+      } else {
+        stringValue = [NSString stringWithFormat:@"%@", value];
+      }
+      if (stringValue) {
+        model[modelProperty] = stringValue;
+      }
+    }
+    else if (realmProperty.type == RLMPropertyTypeDate)
+    {
+      NSDate *date = [NSDate dateFromISO8601String:value];
+      if (date) {
+        model[modelProperty] = date;
+      }
+    }
+    else
+    {
+      model[modelProperty] = value;
+    }
   }
-  return model;
+}
+
++ (id)getValueFromJSON:(NSDictionary *)json
+                atPath:(NSString *)jsonPath
+{
+  NSString *fullJSONPath = jsonPath;
+  NSMutableArray *jsonPathComponents = [NSMutableArray arrayWithCapacity:3];
+  if (![fullJSONPath isEqualToString:@"id"]) {
+    [jsonPathComponents addObject:@"attributes"];
+  }
+  [jsonPathComponents addObjectsFromArray:[fullJSONPath componentsSeparatedByString:@"."]];
+  id value = json;
+  for (NSString *component in jsonPathComponents) {
+    value = [value objectForKey:component];
+    if (!value || value == [NSNull null]) {
+      break;
+    }
+  }
+  return value;
+}
+
++ (void)parseRelationshipsFromJSON:(NSDictionary *)json
+                   intoModelValues:(NSMutableDictionary *)model
+                          forRealm:(RLMRealm *)realm
+               usingJSONtoModelMap:(NSDictionary *)jsonToModelMap
+                   andObjectSchema:(RLMObjectSchema *)objectSchema
+{
+  NSDictionary *links = json[@"relationships"];
+  for (NSDictionary *property in links.keyEnumerator) {
+    NSDictionary *link = links[property];
+    if (!link[@"data"]) {
+      continue;
+    }
+
+    NSString     *modelProperty = jsonToModelMap[property];
+    RLMProperty  *realmProperty = objectSchema[modelProperty];
+    if (!realmProperty) {
+      // Here we have a JSON property with no equivalent model property
+      continue;
+    }
+
+    Class propertyClass = NSClassFromString(realmProperty.objectClassName);
+    if ([link[@"data"] isKindOfClass:[NSDictionary class]])
+    {
+      NSString *uid = link[@"data"][@"id"];
+      id linkedObject = [self realmObjectInRealm:realm withClass:propertyClass andUID:uid];
+      model[modelProperty] = linkedObject;
+    }
+    else if ([link[@"data"] isKindOfClass:[NSArray class]])
+    {
+      NSMutableArray *linkedObjects = [NSMutableArray array];
+      for (id linkageItem in link[@"data"]) {
+        NSString *uid = linkageItem[@"id"];
+        id linkedObject = [self realmObjectInRealm:realm withClass:propertyClass andUID:uid];
+        [linkedObjects addObject:linkedObject];
+      }
+      model[modelProperty] = linkedObjects;
+    }
+    else
+    {
+      // TODO: linkage is not an array or dictionary.. what to do with it?
+    }
+  }
+}
+
++ (id)realmObjectInRealm:(RLMRealm *)realm withClass:(Class)cls andUID:(NSString *)uid {
+  id object = [cls objectInRealm:realm forPrimaryKey:uid];
+  if (object) {
+    return object;
+  } else {
+    NSString *primaryKey = [cls primaryKey];
+    return [cls createInDefaultRealmWithValue:@{ primaryKey: uid }];
+  }
 }
 
 // TODO: construct the json on a bg thread, then put in realm on the main thread
@@ -109,15 +171,6 @@
 
 + (instancetype)createOrUpdateInRealm:(RLMRealm *)realm withJSON:(NSDictionary *)json {
   return [self createOrUpdateInRealm:realm withValue:[self mapJSONToModel:json withRealm:realm]];
-}
-
-+ (id)realmObjectInRealm:(RLMRealm *)realm withClass:(Class)cls andUID:(NSString *)uid {
-  id object = [cls objectInRealm:realm forPrimaryKey:uid];
-  if (object) {
-    return object;
-  } else {
-    return [cls createInDefaultRealmWithValue:@{ @"uid" : uid }];
-  }
 }
 
 @end
